10355ba1961a146e7a06fa6df065716290702eac
liulj355 <liulj355@chinaunicom.cn>
2021-01-27 11:16:36 +0800

    集成mysql和redis

--- spring-security-oauth2/src/main/java/com/cloud/security/springsecurityoauth2/SpringSecurityOauth2Application.java
+++ spring-security-oauth2/src/main/java/com/cloud/security/springsecurityoauth2/SpringSecurityOauth2Application.java
@@ -1,9 +1,13 @@
 package com.cloud.security.springsecurityoauth2;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(value = {"com.cloud"})
+@MapperScan("cn.cloud.**.dao")
 public class SpringSecurityOauth2Application {
 
     public static void main(String[] args) {
--- spring-security/src/main/java/com/cloud/security/springsecurity/SpringSecurityApplication.java
+++ spring-security/src/main/java/com/cloud/security/springsecurity/SpringSecurityApplication.java
@@ -1,9 +1,13 @@
 package com.cloud.security.springsecurity;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(value = {"com.cloud"})
+@MapperScan("cn.cloud.**.dao")
 public class SpringSecurityApplication {
 
     public static void main(String[] args) {
