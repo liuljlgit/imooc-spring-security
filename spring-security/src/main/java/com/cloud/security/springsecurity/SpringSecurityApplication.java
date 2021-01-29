1a377bae83e48676b37a379d77686a6f2bfec307
liulj355 <liulj355@chinaunicom.cn>
2021-01-29 17:19:12 +0800

    生成user表

--- spring-security/src/main/java/com/cloud/security/springsecurity/SpringSecurityApplication.java
+++ spring-security/src/main/java/com/cloud/security/springsecurity/SpringSecurityApplication.java
@@ -1,13 +1,19 @@
 package com.cloud.security.springsecurity;
 
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@ComponentScan(value = {"com.cloud"})
-@MapperScan("cn.cloud.**.dao")
+@EnableAspectJAutoProxy
+@EnableSwagger2
+@EnableKnife4j
+@ComponentScan(value = "com.cloud")
+@MapperScan("com.cloud.**.dao")
 public class SpringSecurityApplication {
 
     public static void main(String[] args) {
